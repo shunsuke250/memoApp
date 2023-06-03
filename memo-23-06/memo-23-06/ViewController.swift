c72652d7677c857779c40ed3a5d82cbfe6cb6415
副山俊輔 <soeyamashunsuke@MacBook-Pro.local>
2023-06-04 01:17:44 +0900

    [add] initial commit

---
+++ memo-23-06/memo-23-06/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  memo-23-06
+//
+//  Created by 副山俊輔 on 2023/06/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
